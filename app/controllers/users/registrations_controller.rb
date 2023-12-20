873155065059b30aef9a571deec0e2b927f11b84
Alexey Chentsov <1007leo@mail.ru>
2023-12-20 23:35:54 +0300

    #17: Translate edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update] # rubocop:disable Rails/LexicallyScopedActionFilter
   layout 'application', only: %i[edit update]
 
   # GET /resource/sign_up
@@ -48,9 +48,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
