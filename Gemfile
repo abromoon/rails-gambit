37a58d5e5bdf18bdd61b324e91d2120c87cf1f79
Evgeniy Falin <original.falin@gmail.com>
2024-02-08 13:01:24 +0300

    chore: Add dotenv gem

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ group :development, :test do
   gem 'awesome_print', '~> 1.8'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'dotenv-rails'
+
   gem 'rubocop'
   gem 'rubocop-performance', require: false
   gem 'rubocop-rails', require: false
--- config/application.rb
+++ config/application.rb
@@ -39,16 +39,16 @@ module RailsGambit
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     config.time_zone = 'Europe/Moscow'
     config.i18n.default_locale = :ru
     config.i18n.available_locales = %i[ru en]
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.fallbacks = true
-    
+
     config.i18n.fallbacks = %i[en]
-    
+
   end
 end
 
