fd4932f6f002ea819c8aa3105241f01f954adffc
Iliia Denisov <iliiadenisov@Iliias-MacBook-Air.local>
2024-05-01 20:22:26 +0300

    Modify helper file a bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,13 +23,13 @@ require 'shoulda/matchers'
 
 require 'simplecov'
 require 'simplecov-console'
-
-SimpleCov.start
+require 'simplecov-cobertura'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-                                                                 SimpleCov::Formatter::HTMLFormatter, # for gitlab
-                                                                 SimpleCov::Formatter::Console # for developers
-                                                               ])
+                                                                SimpleCov::Formatter::HTMLFormatter,# for gitlab
+                                                                SimpleCov::Formatter::Console, # for developers
+                                                                SimpleCov::Formatter::CoberturaFormatter # for test coverage visualization
+                                                              ])
 
 SimpleCov.coverage_dir("coverage/#{ENV.fetch('TEST_TYPE', 'tests')}/")
 
